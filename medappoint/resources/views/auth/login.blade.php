a7f3e747ef544a22ce93939bfbd3ef4b2bf10136
githubjoahan <joahan.alvaro8@gmail.com>
2023-11-21 23:28:43 -0600

    Se agregó ultimos cambios en el login del sistema

--- medappoint/resources/views/auth/login.blade.php
+++ medappoint/resources/views/auth/login.blade.php
@@ -4,25 +4,33 @@
 @section('subtitle', 'Ingresa tus datos para iniciar sesión')
 
 
+
 @section('content')
 <div class="container mt--8 pb-5">
     <div class="row justify-content-center">
         <div class="col-lg-5 col-md-7">
             <div class="card bg-secondary shadow border-0">
+
                 <div class="card-body px-lg-5 py-lg-5">
+
                     @if ($errors->any())
                     <div class="alert alert-danger" role="alert">
                         {{ $errors->first() }}
                     </div>
                     @endif
+
                     <form role="form" method="POST" action="{{ route('login') }}">
                         @csrf
+
+
+
                         <div class="form-group mb-3">
                             <div class="input-group input-group-alternative">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text"><i class="ni ni-email-83"></i></span>
                                 </div>
                                 <input class="form-control" placeholder="Ingresa tu correo electronico" type="email" name="email" value="{{ old('email') }}" required autofocus>
+
                             </div>
                         </div>
                         <div class="form-group">
@@ -36,18 +44,25 @@
                                         <i class="ni ni-eye" id="toggle-password"></i>
                                     </span>
                                 </div>
+
                             </div>
                         </div>
+                        
                         <div class="g-recaptcha" data-sitekey="{{ env('RECAPTCHA_SITE_KEY') }}">
                         </div>
-                        
                         <div class="custom-control custom-control-alternative custom-checkbox">
                             <input name="remember" class="custom-control-input" id="remember" type="checkbox" {{ old('remember') ? 'checked' : '' }}>
                             <label class="custom-control-label" for="remember">
                                 <span class="text-muted">Recordar sesión</span>
                             </label>
                         </div>
-                        <div class="row mt-3">
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary my-4">Ingresar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div class="row mt-3">
            
                  <!--<div class="col-6 text-right">
                     Corrige la ruta del enlace para la recuperación de contraseña  
@@ -64,34 +79,17 @@
                 </div>
 
 
+
                 <div class="col-6 text-right">
                     <a href="{{ route('register') }}" class="text-light">
                         <small>¿Aún no te has registrado?</small>
                     </a>
                 </div>
             </div>
-                        <div class="text-center">
-                            <button type="submit" class="btn btn-primary my-4">Ingresar</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-
-            
-            <!-- funcion poder restablecer contraseñas -->
-            
-            
-           
-      </div>
-
-
-
-
-
         </div>
     </div>
 </div>
-
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         const togglePassword = document.querySelector('#toggle-password');
